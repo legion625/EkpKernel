586c900a66a11133442cc8ed0cde6a5c8e0042f6
Min-Hua Chao <45264512+legion625@users.noreply.github.com>
2022-11-13 19:50:11 +0800

    [change]
    1. Minor changes
      - Adjust the length of 'desp' in ProdMod from 45 to 200.

--- deployment/db/create_stmt.sql
+++ deployment/db/create_stmt.sql
@@ -56,15 +56,6 @@ CREATE TABLE `mbom_part_acq_r_s` (
   PRIMARY KEY (`uid`)
 ) ;
 
-CREATE TABLE `mbom_part_cfg_conj` (
-  `uid` varchar(45) NOT NULL,
-  `part_cfg_uid` varchar(45) DEFAULT NULL,
-  `part_acq_uid` varchar(45) DEFAULT NULL,
-  `object_create_time` bigint(20) DEFAULT NULL,
-  `object_update_time` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`uid`)
-) ;
-
 CREATE TABLE `mbom_part_cfg` (
   `uid` varchar(45) NOT NULL,
   `root_part_uid` varchar(45) DEFAULT NULL,
@@ -73,6 +64,15 @@ CREATE TABLE `mbom_part_cfg` (
   `id` varchar(45) DEFAULT NULL,
   `name` varchar(45) DEFAULT NULL,
   `desp` varchar(200) DEFAULT NULL,
+  `object_create_time` bigint(20) DEFAULT NULL,
+  `object_update_time` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ;
+
+CREATE TABLE `mbom_part_cfg_conj` (
+  `uid` varchar(45) NOT NULL,
+  `part_cfg_uid` varchar(45) DEFAULT NULL,
+  `part_acq_uid` varchar(45) DEFAULT NULL,
   `object_create_time` bigint(20) DEFAULT NULL,
   `object_update_time` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`uid`)
@@ -101,11 +101,9 @@ CREATE TABLE `mbom_prod_ctl` (
   PRIMARY KEY (`uid`)
 ) ;
 
-CREATE TABLE `mbom_prod_mod_item` (
+CREATE TABLE `mbom_prod_ctl_part_cfg_conj` (
   `uid` varchar(45) NOT NULL,
-  `prod_mod_uid` varchar(45) DEFAULT NULL,
   `prod_ctl_uid` varchar(45) DEFAULT NULL,
-  `part_cfg_assigned` tinyint(4) DEFAULT NULL,
   `part_cfg_uid` varchar(45) DEFAULT NULL,
   `object_create_time` bigint(20) DEFAULT NULL,
   `object_update_time` bigint(20) DEFAULT NULL,
@@ -117,30 +115,18 @@ CREATE TABLE `mbom_prod_mod` (
   `prod_uid` varchar(45) DEFAULT NULL,
   `id` varchar(45) DEFAULT NULL,
   `name` varchar(45) DEFAULT NULL,
-  `desp` varchar(45) DEFAULT NULL,
+  `desp` varchar(200) DEFAULT NULL,
   `object_create_time` bigint(20) DEFAULT NULL,
   `object_update_time` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ;
 
-CREATE TABLE `mbom_prod_ctl_part_cfg_conj` (
+CREATE TABLE `mbom_prod_mod_item` (
   `uid` varchar(45) NOT NULL,
+  `prod_mod_uid` varchar(45) DEFAULT NULL,
   `prod_ctl_uid` varchar(45) DEFAULT NULL,
+  `part_cfg_assigned` tinyint(4) DEFAULT NULL,
   `part_cfg_uid` varchar(45) DEFAULT NULL,
-  `object_create_time` bigint(20) DEFAULT NULL,
-  `object_update_time` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`uid`)
-) ;
-
-CREATE TABLE `mbom_prod_ctl` (
-  `uid` varchar(45) NOT NULL,
-  `id` varchar(45) DEFAULT NULL,
-  `lv` tinyint(4) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `req` tinyint(4) DEFAULT NULL,
-  `parent_uid` varchar(45) DEFAULT NULL,
-  `parent_id` varchar(45) DEFAULT NULL,
-  `prod_uid` varchar(45) DEFAULT NULL,
   `object_create_time` bigint(20) DEFAULT NULL,
   `object_update_time` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`uid`)
